728cfa40875d2dddc8806b0061a17086cd78bbcc
Sam Hollyer <rsamhollyer@gmail.com>
2021-05-25 16:10:38 -0500

    stuff

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-'use strict';
+"use strict";
+window.onload = () => {
+  const submit = document.querySelector("#submit");
+  submit.addEventListener("click", handleClick);
+};
+function getTheElements() {
+  const name = document.querySelector("#name");
+  const food = document.querySelector("#food");
+  const animal = document.querySelector("#animal");
+  const spans = document.querySelectorAll("span");
+  return [name, food, animal, spans];
+}
+
+function assignTheValues(array) {
+  let [name, food, animal, spans] = array;
+  spans = Array.from(spans);
+  spans.map((span, idx) => (span.innerText = array[idx].value));
+}
+
+function handleClick(e) {
+  e.preventDefault();
+  const elementsArray = getTheElements();
+  assignTheValues(elementsArray);
+}
